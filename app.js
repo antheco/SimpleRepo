b852d70ba30c0ac295b11ab5c5540a9136a8001a
Anthony Economou <anthony.economou@avanade.com>
2020-10-07 04:35:58 +0900

    trying promise feature out.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+//console.log('Hello World');
+function func1() {
+    console.log('func1 is running');
+};
+var user = new Promise((resolver, reject)=> {
+    console.log('step 1 running');
+    if(!true){
+        resolver(console.log('resolver ran.'));
+    }
+    else {
+        reject(console.log('reject ran'));
+    }
+}).then((data)=> {
+    func1();
+}).catch((e) =>{
+    console.log('error');
+});
